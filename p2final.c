0595cca15b1980a3a56bd9d0ca9afbf81d86751b
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-03-14 07:07:15 +0000

    p2

--- p2final.c
+++ p2final.c
@@ -7,7 +7,7 @@ int input_side()
 }
 int check_scalene(int a,int b,int c)
 {
-  if(a!=b&&a!=c&&b!=c)
+  if(a!=b && a!=c && b!=c)
   {
    return 1; 
   }
@@ -15,8 +15,8 @@ int check_scalene(int a,int b,int c)
 }
 void output(int a,int b,int c, int isscalene)
 {
-  if(is scalene==0)
-}
+  if(isscalene==0)
+  {
   printf("the triangle with side %d,%d and %d is scalene\n",a,b,c);
 }
   else
@@ -26,8 +26,7 @@ void output(int a,int b,int c, int isscalene)
 }
 int main()
 {
-  float a,b,c,isscalene;
-  printf("enter 3 numbers\n");
+  int a,b,c,isscalene;
   a=input_side();
   b=input_side();
   c=input_side();
